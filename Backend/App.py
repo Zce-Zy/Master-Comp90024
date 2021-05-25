3ed397a726977f1fe6ccea1e4000e700145646a8
Zce-Zy <62124908+Zce-Zy@users.noreply.github.com>
2021-05-25 22:36:22 +0800

    Update App.py

--- Backend/App.py
+++ Backend/App.py
@@ -56,4 +56,4 @@ api.add_resource(Lga_Senti_OverviewInd, '/api/Lga_Senti_Overview/<Lganame>')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=80)
