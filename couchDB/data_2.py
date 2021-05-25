3cb0eae78ee31b58de0d98ca85468b3b97f3157d
LI90040 <70268878+LI90040@users.noreply.github.com>
2021-05-26 08:59:22 +1000

    Add files via upload

---
+++ couchDB/data_2.py
@@ -0,0 +1,40 @@
+import couchdb
+import json
+# from datetime import datetime
+
+#connect to couchDB
+def collect_server(user, password):
+    try:
+        couchServer = couchdb.Server('http://%s:%s@172.26.129.241:5984/' % (user, password))
+        print('Couchdb is conllected')
+        return couchServer
+    except Exception as e:
+        print(e)
+
+def get_database(server_name, database_name):
+    if database_name in server_name:
+        db = server_name[database_name]
+        print('get database: %s' % database_name)
+        return db
+    else:
+        #create new database
+        db = server_name.create(database_name)
+        print('Database: %s not found. Created database' % database_name)
+        return db
+
+
+user = 'admin'
+password = '9988'
+
+couchServer = collect_server(user, password)
+db = get_database(couchServer, 'tweets_dic')
+
+old_tweet_file_2 = '/home/ubuntu/comp90024/Master-Comp90024-main/couchDB/old_data_2.json'
+
+with open(old_tweet_file_2) as f:
+    data = json.load(f)
+    print('start upload tweets')
+    for i in range(0, len(data['docs'])):
+        index = i
+        string = data['docs'][index]
+        db.save(string)
